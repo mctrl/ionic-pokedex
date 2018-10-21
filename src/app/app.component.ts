c7f14d9539994033f214a7eea0720887e52189a5
mctrl <mctrl@users.noreply.github.com>
2018-10-21 23:26:31 +0100

    add menu with generations regions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,39 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
+  pages: Array<{title: string}>;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen
+    ) {
+
+    // set our app's pages
+    this.pages = [
+      { title: 'Kanto'},
+      { title: 'Johto'},
+      { title: 'Hoenn'},
+      { title: 'Sinnoh'},
+      { title: 'Unova'},
+      { title: 'Kalos'},
+      { title: 'Alola'}
+
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    // this.menu.close();
+    // navigate to the new page if it is not the current page
+    // this.nav.setRoot(page.component);
+    console.log('trying to navigate to generation', page)
   }
 }
 
